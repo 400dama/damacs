5baa4c967aafd0bed696a226345219875dcedda0
400dama <gotenks9002@gmail.com>
2021-04-30 16:19:36 +0800

    config eaf

--- site-lisp/config/init-eaf.el
+++ site-lisp/config/init-eaf.el
@@ -1,8 +1,7 @@
 ;;; -*- lexical-binding: t -*-
 (require 'eaf)
-;; Make `eaf-browser-restore-buffers' restore last close browser buffers.
-(setq eaf-browser-continue-where-left-off t)
 
+;; keybind
 (eaf-bind-key scroll_up "M-j" eaf-browser-keybinding)
 (eaf-bind-key scroll_down "M-k" eaf-browser-keybinding)
 (eaf-bind-key scroll_up_page "M-n" eaf-browser-keybinding)
@@ -10,6 +9,22 @@
 (eaf-bind-key open_link "M-h" eaf-browser-keybinding)
 (eaf-bind-key open_link_new_buffer "M-H" eaf-browser-keybinding)
 (eaf-bind-key refresh_page "M-r" eaf-browser-keybinding)
+
+;; Make EAF the Default Browser in Emacs
+(setq browse-url-browser-function 'eaf-open-browser)
+(defalias 'browse-web #'eaf-open-browser)
+
+;; Make `eaf-browser-restore-buffers' restore last close browser buffers.
+(setq eaf-browser-continue-where-left-off t)
+
+;; Enable Adblocker
+(eaf-setq eaf-browser-enable-adblocker "true")
+
+;; Default Search Engine
+(setq eaf-browser-default-search-engine "duckduckgo")
+
+;; Default Zoom
+(eaf-setq eaf-browser-default-zoom "1.25")
 
 ;; ------------
 (provide 'init-eaf)
