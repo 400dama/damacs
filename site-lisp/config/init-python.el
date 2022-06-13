dd09ab97ca79798958dc606e2a8c9d2e7bb8d495
400dama <gotenks9002@gmail.com>
2022-06-13 23:14:10 +0800

    python-mode

--- site-lisp/config/init-python.el
+++ site-lisp/config/init-python.el
@@ -1,5 +1,13 @@
 ;;; -*- lexical-binding: t -*-
 
+(use-package python-mode
+  :ensure nil
+  :after flycheck
+  :mode "\\.py\\'"
+  :custom
+  (python-indent-offset 4)
+  (flycheck-python-pycompile-executable "python3")
+  (python-shell-interpreter "python3"))
 
 ;; ------------
 (provide 'init-python)
